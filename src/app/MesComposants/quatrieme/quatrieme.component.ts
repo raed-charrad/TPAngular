e2fe33698416fc5717af3c3e010bd5abf689792f
raed-charrad <raed.charrad91@gmail.com>
2022-11-08 18:47:18 +0100

    changes

---
+++ src/app/MesComposants/quatrieme/quatrieme.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quatrieme',
+  template: `
+    <h2>Quatrième Composant</h2>
+  `,
+  styles: [
+  ]
+})
+export class QuatriemeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PremierComponent } from './premier/premier.component';
+import { DeuxiemeComponent } from './deuxieme/deuxieme.component';
+import { TroisiemeComponent } from './troisieme/troisieme.component';
+import { QuatriemeComponent } from './MesComposants/quatrieme/quatrieme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierComponent,
+    DeuxiemeComponent,
+    TroisiemeComponent,
+    QuatriemeComponent
   ],
   imports: [
     BrowserModule
